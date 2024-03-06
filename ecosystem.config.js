a5c65ae5833198e1f83093182d87447bc28dbb2f
Rajdeep Malakar <rajdeepm.dev@gmail.com>
2024-03-06 17:58:25 +0530

    Update ecosystem.config.js
    Signed-off-by: Rajdeep Malakar <rajdeepm.dev@gmail.com>

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,9 @@
+// This is a script to be used by PM2
+// To use it, run `pm2 start ecosystem.config.js`
+// This script should be used in conjunction with the `check_and_restart.sh` file.
 module.exports = {
   apps : [{
     name   : "Xantic",
-    script : "./index.js",
+    script : "./src/index.js",
   }]
 }
